85a49d31e3636a3faff280b1982a7dea7369cd86
Ritika213 <ritikakgupta6@gmail.com>
2023-11-07 15:18:45 +0530

    task1

---
+++ task1/main.js
@@ -0,0 +1,8 @@
+let header = document.getElementById('header-title');
+let head = document.getElementById('head');
+
+header.style.borderBottom='5px solid black';
+head.style.fontWeight='bold';
+head.style.color='green';
+
+
